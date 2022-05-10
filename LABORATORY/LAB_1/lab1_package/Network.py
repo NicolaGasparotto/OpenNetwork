f81f37b37aafc9dbd435d6e430048a9f1757d96b
Nicola <s258542@studenti.polito.it>
2022-05-10 18:45:26 +0200

    find_path done

--- LABORATORY/LAB_1/lab1_package/Network.py
+++ LABORATORY/LAB_1/lab1_package/Network.py
@@ -1,4 +1,5 @@
 import json
+from itertools import permutations
 
 from math import dist
 
@@ -44,6 +45,31 @@ class Network:
                 self.nodes[node_name].successive[line_name] = self.lines[line_name]
                 self.lines[line_name].successive[connected_node] = self.nodes[connected_node]
 
+    def find_path(self, start_node, end_node):
+        visited = {}
+        for node in self.nodes:
+            visited[node] = False
+        path_nodes = []
+        list_path = []
+
+        def find_path_r(node_s, node_d, visited_nodes, path):
+
+            visited[node_s] = True
+            path.append(node_s)
+
+            if node_s == node_d:
+                # print(path)
+                list_path.append(list(path))
+            else:
+                for node_i in self.nodes[node_s].connected_nodes:
+                    if not visited[node_i]:
+                        find_path_r(node_i, node_d, visited_nodes, path)
+            path.pop()
+            visited_nodes[node_s] = False
+
+        find_path_r(start_node, end_node, visited, path_nodes)
+        return list_path
+
     def propagate(self, signal_information):
         return self._nodes[signal_information.path[0]].propagate(signal_information)
 
@@ -60,12 +86,18 @@ if __name__ == "__main__":
     network1 = Network('../nodes.json')
     network1.connect()
 
+    print(len(network1.find_path('F', 'C')))
+    # print(len(network1.nodes))
+    # print(len(list(permutations(network1.nodes.keys(), 2))))  # this is necessary for the main in point 5
+    # perm = permutations()
+
+    """
     for line in network1.nodes['A'].successive:
         print(network1.nodes['A'].successive[line])
 
     for node in network1.lines['AC'].successive:
         print(network1.lines['AC'].successive[node])
-    """
+    
     print(type(network1.nodes['A']))  # instance of node as object
 
     for node in network1.nodes:
